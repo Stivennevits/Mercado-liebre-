a09b469b7ab253af1f502cbbf2d41f8563817146
stivennevits <stivennevits16@gmail.com>
2022-10-07 13:30:15 -0500

    media queris actualizadas, funcional en tablet y desktop

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require("express")
 const path = require("path")
 const app = express()
 const publicPath = path.resolve(__dirname, "./public")
+const PORT = process.env.PORT || 4020
 app.use(express.static(publicPath))
 
-app.listen(4020, () =>{console.log("Corriendo servidor en el puerto 3090 http://localhost:4020") } )
+app.listen(PORT, () =>{console.log("Corriendo servidor en el puerto" + " " + PORT + " " + "http://localhost:" + PORT) } )
 
 app.get("/", function(req, res){
     res.sendFile(path.join(__dirname , "./views/home.html"))
